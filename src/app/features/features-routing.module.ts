b7873b477a4e9a53314c82993ba81859927326c5
idrees <muhammad.idrees@gecurrent.com>
2022-06-05 20:30:35 +1000

    Add basic children routing for feature module

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AdvanceComponent } from "./advance/advance.component";
+import { BasicComponent } from "./basic/basic.component";
 import { FeaturesHomeComponent } from "./features-home/fetures-home.component";
+import { ProComponent } from "./pro/pro.component";
 
-const routes: Routes = [{ path: "", component: FeaturesHomeComponent }];
+const routes: Routes = [
+  {
+    path: "",
+    component: FeaturesHomeComponent,
+    children: [
+      { path: "", component: BasicComponent },
+      { path: "advance", component: AdvanceComponent },
+      { path: "pro", component: ProComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
